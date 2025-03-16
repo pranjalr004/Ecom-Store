fa0c09953831f292ac558ec48d68df22e2c67c95
pranjalr004 <pranjalr376@gmail.com>
2025-03-16 16:27:58 +0530

    User route added

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 
 // Utilities
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 4000;
@@ -19,9 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+app.use("/api/users",userRoutes)
 
 
 app.listen(port, ()=>console.log(`Server running on port: ${port}`))
