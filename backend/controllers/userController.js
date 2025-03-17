8a305e635c7f661eab0d8cf98feb588563d85a1f
pranjalr004 <pranjalr376@gmail.com>
2025-03-17 07:48:25 +0530

    AuthMiddleware is created for admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -56,5 +56,12 @@ const loginUser=asyncHandler(async(req,res)=>{
     }
 })
 
+const logoutCurrentUser=asyncHandler(async(req,res)=>{
+    res.cookie('jwt','',{
+        httpOnly:true,
+        expires:new Date(0),
+    })
+    res.status(200).json({message:"Logged out successfully"})
+})
 
-export {createUser, loginUser} 
+export {createUser, loginUser,logoutCurrentUser} 
